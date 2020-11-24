cda0b62d423e2c4ca9c08b42341e3d97ca55d9e3
Luis Miguel Del Gallego <alucardluis@gmail.com>
2020-11-24 07:17:09 -0500

    implement Swiper Card

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hola Mundo !!!!!!!'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [_swiperCards()],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperCards() {
+    return Container(
+      padding: EdgeInsets.only(top: 10),
+      width: double.infinity,
+      height: 300,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            'https://picsum.photos/300/300?random=1',
+            fit: BoxFit.fill,
+          );
+        },
+        layout: SwiperLayout.STACK,
+        itemWidth: 200,
+        itemCount: 3,
+        // autoplay: true,
+        // pagination: new SwiperPagination(),
+        // control: new SwiperControl(),
+      ),
     );
   }
 }
